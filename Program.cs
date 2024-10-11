aa654bc3c0ebf7e537ab5c68e04495f00b15ed03
jayfabinaotumbokon <94710708+jayfabinaotumbokon@users.noreply.github.com>
2024-10-11 11:42:48 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using QueueingSystem.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<QueueingContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
